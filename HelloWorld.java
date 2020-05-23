2843eba6d995e42d7ceadd18b868f2501b5195f0
njustXZ <1765474636@qq.com>
2020-05-23 21:13:15 +0800

    branch1 commit

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+//public表示类
+//class表示定义一个类
+//HelloWorld表示一个类名
+public class HelloWorld{//表示一个公开的类，起名HelloWorld
+	/*
+	   public表示公开的
+	   static表示静态的
+	   void表示空
+	   main表示方法名是main
+
+	 */
+	public static void main(String[]args){//表示定义一个公开的静态主方法
+		//方法体
+		//方法体
+		//方法体
+		//方法体
+
+		//java语句【java语句必须以"；"终止，分号半角】
+		System.out.println("Hello World!");
+	}
+}
